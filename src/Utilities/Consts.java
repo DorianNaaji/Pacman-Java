89b45805a70cee23a64ebc856255ee995f91f00b
Dorian <dorian.naaji@gmail.com>
2019-03-31 14:08:29 +0200

    Changed files location & loading paths

--- src/Utilities/Consts.java
+++ src/Utilities/Consts.java
@@ -13,71 +13,71 @@ public class Consts
 {
     public static String getMapTxtPath()
     {
-        return "src/resources/map.txt";
+        return "resources/map.txt";
     }
     
     public static String getSmallGumImgPath()
     {
-        return "resources/default_small_gum.png";
+        return "file:resources/default_small_gum.png";
     }
     
     public static String getWallImgPath()
     {
-        return "resources/wall.png";
+        return "file:resources/wall.png";
     }
     
     public static String getBigGumImgPath()
     {
-        return "resources/default_big_gum.png";
+        return "file:resources/default_big_gum.png";
     }
     
     public static String getPacmanRightImgPath()
     {
-        return "resources/pacman_right.png";
+        return "file:resources/pacman_right.png";
     }
     
     public static String getPacmanLeftImgPath()
     {
-        return "resources/pacman_left.png";
+        return "file:resources/pacman_left.png";
     }
     
     public static String getPacmanUpImgPath()
     {
-        return "resources/pacman_up.png";
+        return "file:resources/pacman_up.png";
     }
         
     public static String getPacmanDownImgPath()
     {
-        return "resources/pacman_down.png";
+        return "file:resources/pacman_down.png";
     }
     
     public static String getDefaultImgPath()
     {
-        return "resources/default.png";
+        return "file:resources/default.png";
     }
     
     public static String getPolytechImgPath()
     {
-        return "resources/polytech.png";
+        return "file:resources/polytech.png";
     }
     
     public static String getRedGhostImgPath()
     {
-        return "resources/red_ghost.png";
+        return "file:resources/red_ghost.png";
     }
     
     public static String getPinkGhostImgPath()
     {
-        return "resources/pink_ghost.png";
+        return "file:resources/pink_ghost.png";
     }
     
     public static String getOrangeGhostImgPath()
     {
-        return "resources/orange_ghost.png";
+        return "file:resources/orange_ghost.png";
     }
     
     public static String getAfraidGhostImgPath()
     {
-        return "resources/eatable_ghost.png";
+        return "file:resources/eatable_ghost.png";
     }
 }
